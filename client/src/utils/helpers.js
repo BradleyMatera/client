d9e91ec12317c88bc207e325f1f4daae206ce06d
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-09-08 16:40:14 -0500

    .

--- client/src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-export const formatPrice = (price) => {
-  return `$${price.toFixed(2)}`;
-};
-
-export const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
